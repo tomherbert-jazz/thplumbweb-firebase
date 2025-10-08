dce40653c9d92b1d14817e7ff5bc7036f31a9294
Tom Herbert <tomherbertjazz@gmail.com>
2025-10-08 02:58:28 +0000

    https://tomwebapp22.azurewebsites.net/Pictures/r.jpg    ... background

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'tomwebapp22.azurewebsites.net',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
